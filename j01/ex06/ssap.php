43e36548f98f3704c07943e9dd9c58b1b88dc3a3
Max CANAL <mcanal@e2r5p6.42.fr>
2015-04-07 20:44:52 +0200

    youplaboom

--- j01/ex06/ssap.php
+++ j01/ex06/ssap.php
@@ -1,17 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hw.php                                             :+:      :+:    :+:   */
+/*   ssap.php                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcanal <zboub@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/04/07 20:35:55 by mcanal            #+#    #+#             */
-/*   Updated: 2015/04/07 20:36:02 by mcanal           ###   ########.fr       */
+/*   Created: 2015/04/07 20:37:06 by mcanal            #+#    #+#             */
+/*   Updated: 2015/04/07 20:37:12 by mcanal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #!/usr/bin/php
 
 <?php
-echo "Hello World\n";
+$i = 0;
+$a = array();
+while($i++ < $argc - 1)
+{
+	$argv[$i] = trim($argv[$i]);
+	while(strpos($argv[$i], "  "))
+	$argv[$i] = str_replace("  ", " ", $argv[$i]);
+	$tmp = explode(" ", $argv[$i]);
+	$a = array_merge($a, $tmp);
+}
+sort($a);
+foreach($a as $tmp)
+	echo "$tmp\n";
 ?>
--- j01/ex08/ft_is_sort.php
+++ j01/ex08/ft_is_sort.php
@@ -1,17 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   hw.php                                             :+:      :+:    :+:   */
+/*   ft_is_sort.php                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mcanal <zboub@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2015/04/07 20:35:55 by mcanal            #+#    #+#             */
-/*   Updated: 2015/04/07 20:36:02 by mcanal           ###   ########.fr       */
+/*   Created: 2015/04/07 20:37:27 by mcanal            #+#    #+#             */
+/*   Updated: 2015/04/07 20:37:34 by mcanal           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #!/usr/bin/php
 
 <?php
-echo "Hello World\n";
+function ft_is_sort($tab)
+{
+    $len = count($tab);
+    $sorted = $tab;
+    sort($sorted);
+
+    for ($i = 0; $i < $len; $i++)
+        if (!strcmp($tab[$i], $sorted[$i]))
+			return false;
+	return true;
+}
 ?>
