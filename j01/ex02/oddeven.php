867235283d1727435ac54b97789b4d90e0b51359
Max CANAL <mcanal@e2r5p6.42.fr>
2015-04-07 18:29:46 +0200

    j01

---
+++ j01/ex02/oddeven.php
@@ -0,0 +1,16 @@
+#!/usr/bin/php
+<?php
+echo "Entrez un nombre: ";
+while (($nb = fgets(STDIN)))
+{
+	$nb = trim($nb);
+	if (!ctype_digit($nb))
+		echo "'$nb' n'est pas un chiffre\n";
+	else if (!($nb % 2))
+		echo "Le chiffre $nb est Pair\n";
+	else
+		echo "Le chiffre $nb est Impair\n";
+	echo "Entrez un nombre: ";
+}
+echo "^D\n";
+?>
