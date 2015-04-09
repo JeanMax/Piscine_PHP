fdbbe28f1e273daca39efbb1dc4c74e04bf339a5
Max CANAL <mcanal@e2r7p1.42.fr>
2015-04-09 11:53:38 +0200

    who_hou

--- j02/ex03/who.php
+++ j02/ex03/who.php
@@ -1,22 +1,31 @@
 #!/usr/bin/php
 <?php
-/*
-- lire le fichier /var/run/utmpx dans une chaine
-- ignorer le header qui doit faire 1256 bytes
-- boucler tant qu'on est pas à la fin de la chaine :
-- utiliser ce format bien moche pour unpack une structure utmpx : 
-       "a256login/a4id/a32line/ipid/itype/C8time/a256host/I16pad"
-- dans le champs login et line on a 2 des 3 infos qui nous interessent
-- ensuite dans les champs time[1-9] on a les 8 octets qui composent la structure timeval. Les 4 premiers sont ceux des secondes. avec strftime et ces secondes on peut formater la date qui nous interesse.
-- stocker l'entree dans un tableau si le login correspond à get_current_user()
-- avancer de 628 bytes dans la chaine
-- trier le tableau
-*/
+date_default_timezone_set('Europe/Paris');
+$fd = fopen("/var/run/utmpx", "r");
+fread($fd, 1256);
+$i = 0;
+while (!feof($fd))
+{
+	$str = fread($fd, 628);
+	if (strlen($str) > 1)
+	{
+		$pack = unpack("a256user/a4id/a32line/ipid/itype/i2time/a256host/@", $str);
 
+		if (!strncmp($pack["user"], get_current_user(), strlen(get_current_user())))
+		{
+			$tab[$i]["tty"] = $pack["line"];
+			$tab[$i]["user"] = $pack["user"];
+			$tab[$i]["date"] = strftime("%b %e %H:%M",$pack["time1"]);
+			$i++;
+		}
+	}
+}
 
+sort($tab);
+$len = $i;
+for ($i = 0; $i < $len; $i++)
+	echo $tab[$i]["user"]."	".$tab[$i]["tty"]."	".$tab[$i]["date"]."\n";
 
+fclose($fd);
 
-ou autres clefs :
-"a256login/a4id/a32line/ipid/itype/i2time/a256host/i16pad"
-"a256user/a4id/a32line/ipid/itype/I2time/a256host/@"
 ?>
