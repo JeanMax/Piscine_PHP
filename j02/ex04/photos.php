3cb31a6a4f8b91cc0196421b8cb40af741ce95a3
Max CANAL <mcanal@e2r7p1.42.fr>
2015-04-09 19:48:04 +0200

    yay

--- j02/ex04/photos.php
+++ j02/ex04/photos.php
@@ -24,30 +24,29 @@ if (strstr($argv[1], "http"))
 			if (strstr($matches[1][$i], "http"))
 				$tab[$i] = $matches[1][$i];
 			else if ($matches[1][$i][0] == '/' && $matches[1][$i][1] == '/')
-                $tab[$i] = "http:".$matches[1][$i];
+				$tab[$i] = "http:".$matches[1][$i];
 			else if ($matches[1][$i][0] == '/')
 				$tab[$i] = $url.$matches[1][$i];
 			else
 				$tab[$i] = $url."/".$matches[1][$i];
 		}
 
-//        print_r($tab);
-//        print_r($matches);
-        
 		//make directory
-        if (strstr($argv[1], "https"))
-            $path = substr($argv[1], 8)."/";
-        else
-            $path = substr($argv[1], 7)."/";
-            
+		if (strstr($argv[1], "https"))
+			$path = substr($argv[1], 8);
+		else
+			$path = substr($argv[1], 7);
+
+        
 		if (!is_dir($path))
 			mkdir($path, 0755, true);
 
 		//getting images
 		for ($i = 0; $i < $len; $i++)
 		{
+			if (!($matches[1][$i]))
+                continue ;
 			$ci = curl_init($tab[$i]);
-//			curl_setopt($ci, CURLOPT_SSL_VERIFYPEER, 0);
 			curl_setopt($ci, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ci, CURLOPT_HEADER, 0);
 			curl_setopt($ci, CURLOPT_BINARYTRANSFER,1);
