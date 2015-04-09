21b8975c8149521b02b140ddb4a9fcaff0c51003
Max CANAL <mcanal@e2r7p1.42.fr>
2015-04-09 15:43:07 +0200

    zboub

--- j02/ex02/loupe.php
+++ j02/ex02/loupe.php
@@ -2,22 +2,23 @@
 <?php
 $handle = fopen($argv[1], "rw");
 $content = fread($handle, filesize($argv[1]));
-preg_match_all('/"([^"]+)"/', $content, $m);
-preg_match_all('/<a(.*?)>(.*?)<\/a>/si', $content, $m2);
+preg_match_all('/<a(.*?)>(.*?)<\/a>/si', $content, $m);
 $i = 0;
-while ($m2[2][$i])
+while ($m[2][$i])
 {
-    preg_match('/\<([^>]+)\>/' , $m2[2][$i], $link_content);
-    $m2[2][$i] = str_replace($link_content, "", $m2[2][$i]);
+    preg_match_all('/\<([^>]+)\>/' , $m[2][$i], $link_content);
+    $content = str_replace($m[2][$i], strtoupper($m[2][$i]), $content);
+    for ($j = 0; $j < count($link_content[0]); $j++)
+		$content = str_replace(strtoupper($link_content[0][$j]), strtolower($link_content[0][$j]), $content);
 	$i++;
 }
-foreach($m[0] as $el)
+$i = 0;
+while ($m[0][$i])
 {
-	$content = str_replace($el, strtoupper($el), $content);
-}
-foreach($m2[2] as $el)
-{
-	$content = str_replace($el, strtoupper($el), $content);
+	preg_match_all('/title="(.*?)"/', $m[0][$i], $title);
+	for ($j = 0; $j < count($title[1]); $j++)
+		$content = str_replace($title[1][$j], strtoupper($title[1][$j]), $content);
+	$i++;
 }
 echo($content);
 ?>
