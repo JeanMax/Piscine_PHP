7bdb9919b137289afc36f72dc6f7972e00667933
Max CANAL <mcanal@e2r7p1.42.fr>
2015-04-09 18:22:36 +0200

    zboub

--- j02/ex05/deni.php
+++ j02/ex05/deni.php
@@ -18,15 +18,35 @@ if ($argv[2])
 			fclose($fd);
 
 			//edit tab
+            switch ($argv[2])
+            {
+            case "nom" :
+                $choice = 0;
+                break;
+            case "prenom" :
+                $choice = 1;
+                break;
+            case "mail" :
+                $choice = 2;
+                break;
+            case "IP" :
+                $choice = 3;
+                break;
+            case "pseudo" :
+                $choice = 4;
+                break;
+            default :
+                $choice = 0;                
+            }
 			for ($i = 0; $tab[$i]; $i++)
 			{
-				$nom[$tab[$i][1]] = $tab[$i][0];
-				$prenom[$tab[$i][1]] = $tab[$i][1];
-				$mail[$tab[$i][1]] = $tab[$i][2];
-				$IP[$tab[$i][1]] = $tab[$i][3];
-				$pseudo[$tab[$i][1]] = $tab[$i][4];
+				$nom[$tab[$i][$choice]] = $tab[$i][0];
+				$prenom[$tab[$i][$choice]] = $tab[$i][1];
+				$mail[$tab[$i][$choice]] = $tab[$i][2];
+				$IP[$tab[$i][$choice]] = $tab[$i][3];
+				$pseudo[$tab[$i][$choice]] = $tab[$i][4];
 			}
-
+            
 			//asking
 			echo "Entrez votre commande: ";
 			while (($cmd = fgets(STDIN)))
