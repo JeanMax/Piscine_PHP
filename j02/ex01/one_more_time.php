488475ca660f29e6c02ab1db1aa4b3ee490ff423
Max CANAL <mcanal@e2r7p1.42.fr>
2015-04-09 12:44:40 +0200

    yay

--- j02/ex01/one_more_time.php
+++ j02/ex01/one_more_time.php
@@ -1,4 +1,71 @@
 #!/usr/bin/php
 <?php
+functionconv_month_fr($s)
+{
+    if (!strcasecmp($s, "janvier"))
+        $s = "January";
+    else if (!strcasecmp($s, "fevrier"))
+        $s = "February";
+    else if (!strcasecmp($s, "mars"))
+		$s = "March";
+	else if (!strcasecmp($s, "avril"))
+		$s = "April";
+	else if (!strcasecmp($s, "mai"))
+		$s = "May";
+	else if (!strcasecmp($s, "juin"))
+		$s = "June";
+	else if (!strcasecmp($s, "juillet"))
+		$s = "July";
+	else if (!strcasecmp($s, "aout"))
+		$s = "August";
+	else if (!strcasecmp($s, "septembre"))
+		$s = "September";
+	else if (!strcasecmp($s, "octobre"))
+		$s = "October";
+	else if (!strcasecmp($s, "novembre"))
+		$s = "November";
+	else if (!strcasecmp($s, "decembre"))
+		$s = "December";
+	else
+		$s = NULL;
+	return ($s);
+}
 
+functionconv_sec($s)
+{
+	$time = explode(":", $s);
+	if ($time[0] >= 0 && $time[0] <= 23
+		&& $time[1] >= 0 && $time[1] <= 59
+		&& $time[2] >= 0 && $time[2] <= 59)
+		$sec = $time[0] * 3600 + $time[1] * 60 + $time[2];
+	return ($sec);
+}
+
+functionputerror()
+{
+	echo("Wrong Format\n");
+	exit();
+}
+
+if ($argc != 2)
+	puterror();
+$date = explode(" ", "$argv[1]");
+if (count($date) > 5)
+	puterror();
+date_default_timezone_set('Europe/Paris');
+$days = array("lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche");
+if (!in_array(strtolower($date[0]), $days))
+	puterror();
+if ($date[1] > 31)
+	puterror();
+$month = conv_month_fr($date[2]);
+if (!$month)
+	puterror();
+$format = $date[1] . " $month" . " $date[3]";
+$sec = conv_sec($date[4]);
+if (!$sec)
+	puterror();
+$time = strtotime($format);
+$time += $sec;
+echo($time);
 ?>
--- j02/ex02/loupe.php
+++ j02/ex02/loupe.php
@@ -1,4 +1,23 @@
 #!/usr/bin/php
 <?php
-
+$handle = fopen($argv[1], "rw");
+$content = fread($handle, filesize($argv[1]));
+preg_match_all('/"([^"]+)"/', $content, $m);
+preg_match_all('/<a(.*?)>(.*?)<\/a>/si', $content, $m2);
+$i = 0;
+while ($m2[2][$i])
+{
+    preg_match('/\<([^>]+)\>/' , $m2[2][$i], $link_content);
+    $m2[2][$i] = str_replace($link_content, "", $m2[2][$i]);
+	$i++;
+}
+foreach($m[0] as $el)
+{
+	$content = str_replace($el, strtoupper($el), $content);
+}
+foreach($m2[2] as $el)
+{
+	$content = str_replace($el, strtoupper($el), $content);
+}
+echo($content);
 ?>
